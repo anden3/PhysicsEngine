7e6203eebb83933cd2917aa7347f93c5cde87cbe
André Vennberg <andre.vennberg@gmail.com>
2019-09-19 18:02:37 +0200

    Added different types of links.

---
+++ Assets/Scripts/Links/ParticleCable.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Based on code by Ian Millington in Game Physics Engine Development.
+ *
+ * Written by André Vennberg, Sebastian Karlsson & Sara Uvalic.
+ */
+
+using UnityEngine;
+
+using System.Collections.Generic;
+
+public class ParticleCable : ParticleLink
+{
+    [Header("Cable Settings")]
+    public float maxLength;
+    public float restitution;
+
+    public override void GetContacts(ref List<ParticleContact> contacts)
+    {
+        float length = currentLength;
+
+        // Only apply force if overextended.
+        if (length < maxLength) return;
+
+        contacts.Add(new ParticleContact
+        {
+            first = first,
+            second = second,
+            restitution = restitution,
+            penetration = length - maxLength,
+            contactNormal = (second.position - first.position).normalized
+        });
+    }
+}
---
+++ Assets/Scripts/Links/ParticleLink.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Based on code by Ian Millington in Game Physics Engine Development.
+ *
+ * Written by André Vennberg, Sebastian Karlsson & Sara Uvalic.
+ */
+
+using UnityEngine;
+
+using System.Collections.Generic;
+
+public class ParticleLink : MonoBehaviour, IParticleContactGenerator
+{
+    [Header("Endpoints")]
+    public Particle first;
+    public Particle second;
+
+    protected float currentLength
+        => Vector3.Distance(first.position, second.position);
+
+    private void Start() => ParticleContactResolver.Register(this);
+    private void OnDestroy() => ParticleContactResolver.Unregister(this);
+
+    public virtual void GetContacts(ref List<ParticleContact> contacts) { }
+}
