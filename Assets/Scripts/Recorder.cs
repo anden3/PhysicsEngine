33816d450f70e764e2b0f2eafb4b005d22a6727f
André Vennberg <andre.vennberg@gmail.com>
2020-02-05 14:25:55 +0100

    Added recorder scripts for logging.

---
+++ Assets/Scripts/Recorder.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+public class Recorder : MonoBehaviour
+{
+    public string folderName = "Assets/Resources";
+
+    private static string folderPath;
+    private static Recorder instance;
+
+    private static Dictionary<string, FileStream> files = new Dictionary<string, FileStream>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        folderPath = folderName;
+    }
+
+    public static void LogData(string file, params object[] data)
+    {
+        if (instance == null)
+            return;
+
+        if (!files.ContainsKey(file))
+        {
+            files.Add(file, File.Open(
+                $"{folderPath}/{file}.txt",
+                FileMode.Create, FileAccess.Write
+            ));
+        }
+
+        byte[] bytes = new UTF8Encoding(true).GetBytes(
+            $"{string.Join("\t", data)}\n"
+        );
+
+        files[file].Write(bytes, 0, bytes.Length);
+    }
+
+    private void OnDestroy()
+    {
+        foreach (FileStream fs in files.Values)
+        {
+            fs.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/TargetRecorder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TargetRecorder : MonoBehaviour
+{
+    public string fileName;
+
+    private void FixedUpdate()
+    {
+        Recorder.LogData(fileName, Time.fixedTime, transform.localPosition.y - 0.5f);
+    }
+}
